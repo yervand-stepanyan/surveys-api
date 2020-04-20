87157a2a7b6a1171f11f2c346bff2c17ef4fefd0
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-04-20 19:29:30 +0400

    Add app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const keys = require('./config/variables.config')
+
+const app = express();
+
+mongoose.connect(keys.MONGO_URI, {
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useNewUrlParser: true
+}).then(console.log('MongoDB connected'))
+  .catch(error => console.log(error));
+
+app.use(require('morgan')('dev'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
