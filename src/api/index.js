472cb1668fdb8c9c2b7b543a3c5f95cbbe65b325
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-04-23 21:28:38 +0400

    Refactor variable names

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,12 @@
 const app = require('express')()
+
 const surveys = require('./surveys/surveys.api')
-const surveysAnswers = require('./surveys/surveys.api')
+const surveyAnswers = require('./survey-answers/survey-answers.api')
 
 /**
  * @description Add required APIs.
  */
 app.use('/surveys', surveys)
-app.use('/survey-answers', surveysAnswers)
+app.use('/survey-answers', surveyAnswers)
 
 module.exports = app
