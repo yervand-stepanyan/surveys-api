7be8337a7d536e2f6de43951681721e6d8b67cc0
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-04-24 11:23:34 +0400

    Remove ids from survey-answer

--- src/model/schema/survey-answer.schema.js
+++ src/model/schema/survey-answer.schema.js
@@ -1,17 +1,28 @@
 const mongoose = require('mongoose')
+const mongooseToJson = require('@meanie/mongoose-to-json')
 
 /**
- * @description Define the SurveyAnswers schema.
+ * @description Define the SurveyAnswerEntry schema.
+ */
+const SurveyAnswerEntrySchema = new mongoose.Schema({
+  customAnswer: { type: String, maxlength: 1000 },
+  markedAnswers: [
+    { type: String, maxlength: 100 }
+  ],
+  questionId: { type: String, maxlength: 100, required: true }
+}, { _id: false })
+
+/**
+ * @description Define the SurveyAnswer schema.
  */
 const SurveyAnswerSchema = new mongoose.Schema({
-  surveyId: {type: mongoose.Schema.Types.ObjectId, ref: 'Surveys', required: true},
-  answers: [{
-    customAnswer: {type: String, maxlength: 1000},
-    markedAnswers: [
-      {type: String, maxlength: 100}
-    ],
-    questionId: {type: String, maxlength: 100, required: true}
-  }]
-}, {timestamps: true})
+  surveyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Surveys', required: true },
+  answers: [SurveyAnswerEntrySchema]
+}, { timestamps: true })
+
+/**
+ * @description Set plugins: Transform _id to id.
+ */
+SurveyAnswerSchema.plugin(mongooseToJson)
 
 module.exports = SurveyAnswerSchema
--- src/model/schema/survey.schema.js
+++ src/model/schema/survey.schema.js
@@ -5,36 +5,36 @@ const mongooseToJson = require('@meanie/mongoose-to-json')
  * @description Define the SurveyQuestionAnswer schema.
  */
 const SurveyQuestionAnswerSchema = new mongoose.Schema({
-  id: {type: String, maxlength: 100},
-  title: {type: String, maxlength: 1000}
-}, {_id: false})
+  id: { type: String, maxlength: 100 },
+  title: { type: String, maxlength: 1000 }
+}, { _id: false })
 
 /**
  * @description Define the SurveyQuestion schema.
  */
 const SurveyQuestionSchema = new mongoose.Schema({
-    id: {type: String, maxlength: 100, required: true},
-    title: {type: String, maxlength: 1000, required: true},
-    hasLastInput: {type: Boolean, required: true},
-    answerType: {
-      type: String,
-      enum: ['INPUT', 'RADIOBUTTON', 'CHECKBOX', 'DROPDOWN', 'RANGE'],
-      required: true
-    },
-    answers: [SurveyQuestionAnswerSchema],
-    startValue: {type: String, maxlength: 100},
-    endValue: {type: String, maxlength: 100},
-    stepValue: {type: String, maxlength: 100},
-    inputType: {type: String, maxlength: 100}
-  }, {_id: false})
+  id: { type: String, maxlength: 100, required: true },
+  title: { type: String, maxlength: 1000, required: true },
+  hasLastInput: { type: Boolean, required: true },
+  answerType: {
+    type: String,
+    enum: ['INPUT', 'RADIOBUTTON', 'CHECKBOX', 'DROPDOWN', 'RANGE'],
+    required: true
+  },
+  answers: [SurveyQuestionAnswerSchema],
+  startValue: { type: String, maxlength: 100 },
+  endValue: { type: String, maxlength: 100 },
+  stepValue: { type: String, maxlength: 100 },
+  inputType: { type: String, maxlength: 100 }
+}, { _id: false })
 
 /**
  * @description Define the Survey schema.
  */
 const SurveySchema = new mongoose.Schema({
-  title: {type: String, maxlength: 100, required: true},
+  title: { type: String, maxlength: 100, required: true },
   questions: [SurveyQuestionSchema]
-}, {timestamps: true})
+}, { timestamps: true })
 
 /**
  * @description Set plugins: Transform _id to id.
